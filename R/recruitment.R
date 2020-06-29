0388cf5d8030599a7363fd927aadb5bb8f2ff966
Maia Sosa Kapur <kapurm@uw.edu>
2020-06-29 12:16:53 -0700

    getEqRec accepts eq method

--- R/recruitment.R
+++ R/recruitment.R
@@ -16,15 +16,17 @@ doSRR <- function(SB_aiy, SB0i = SB0_i, h = steep, R0=1){
   R_yi <- 4*h*R0*SB_aiy/(SB0i*(1-h)+ SB_aiy*(5*h-1))
   return(R_yi)
 }
+
 getEqRec <- function(SRR = 1, h = steep, 
                      Fv = rep(0,narea), 
-                     gam = 1, R0 = 1, S0 = 0.6739975){
+                     gam = 1, R0 = 1, S0 = 0.6739975,
+                     ...){
   sumSBPR <- R <- NULL
   
   for(i in 1:narea){
     
     ## get s-tilde
-    sumSBPR[i] <- sum(doNage(Fv = Fv)[,9+i]) ## SB in cols 10:12
+    sumSBPR[i] <- sum(doNage(Fv = Fv,eq_method = eq_method)[,9+i]) ## SB in cols 10:12
     # sumSBPR <- sum(sbpr)
     # if(SRR == 1){ ## bevholt
     
