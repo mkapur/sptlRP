8268c57d1e13c6c018efb0d7def6abac4c8ee106
Maia Sosa Kapur <kapurm@uw.edu>
2020-06-29 12:16:47 -0700

    accepts eq method

--- R/doYPR.R
+++ R/doYPR.R
@@ -1,11 +1,11 @@
-doYPR <-function( Fv= rep(0.2,narea), M = 0.15 ) {
+doYPR <-function( Fv= rep(0.2,narea), M = 0.15, ...) {
   ypr_sa <- array(NA, dim = c(1,2,narea)) ## each sex by area
   ypr_a <- matrix(NA, nrow = 1, ncol = narea) ## each  area
   ypr  <- NA  ## total
   for(i in 1:narea){
     for(s in 1:2){ ## loop sexes
       wt <- dat[,s+2,i] ## cols 2 & 3
-      nzmat <-  doNage(s = s, Fv = Fv) ## expects 3 FV values
+      nzmat <-  doNage(s = s, Fv = Fv, eq_method = eq_method) ## expects 3 FV values
       n_a <- nzmat[,i]
       z_a <- nzmat[,i+3]
       ## Baranov
