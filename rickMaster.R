55188b35096221b34093641803ea90c0de0f13db
Maia Sosa Kapur <kapurm@uw.edu>
2020-07-02 12:33:51 -0700

    about to add in new RLevel

--- rickMaster.R
+++ rickMaster.R
@@ -26,7 +26,7 @@ SB0 <- doNage( Fv = rep(0,narea),
 ## Get NAA using movement. Input X_ija_NULL to turn movement OFF (smooth curve)
 ## applying system-wide F
 current <- data.frame(Fv = NA, Yield = NA, B = NA)
-Ftest <- seq(0,1,0.05)
+Ftest <- seq(0,1,0.005)
 for(v in 1:length(Ftest)){
   
   ## get values at present Fv
@@ -44,7 +44,7 @@ for(v in 1:length(Ftest)){
                                  Recr_virgin = R0, SPR_temp = SB_R)## L17247 ON TPL
   current[v,'Fv'] <- Ftest[v]
   current[v,'Yield'] <- Yield_R * currEq$R_equil
-  current[v,'B'] <- SB_R*currEq$B_equil
+  current[v,'B'] <- SB_R* currEq$R_equil#*currEq$B_equil
 }
 
 
@@ -53,7 +53,7 @@ for(v in 1:length(Ftest)){
 ## proposed approach ----
 
 ## applying system-wide F
-Ftest <- seq(0,1,0.005)
+
 proposed <- data.frame(Fv = NA, Yield = NA, B = NA)
 proposed_i <- array(NA, dim = c(length(Ftest),3,narea), dimnames = list(NULL,c('Fv','Yield',"B"))) ## now for each area
 B_eq_i <- R_eq_i <-SB_Ri <- Yield_Ri<- matrix(NA, nrow =length(Ftest), ncol = narea)
@@ -63,9 +63,11 @@ for(v in 1:length(Ftest)){
   for(k in 1:2){ ## Loop over steps A & B
     
     if(k == 1){
-      rUse <- recr_dist
+      rdistUse <- recr_dist
+      rlevelUse <- rec_level
     } else{
-      rUse <- recr_dist_adj
+      rdistUse <- recr_dist_adj
+      rlevelUse <- sum(  R_eq_i[v,])
     }
     ## define virgin biomass by AREA
     SB0_i <- doNage( Fv = rep(0,narea), 
@@ -83,28 +85,29 @@ for(v in 1:length(Ftest)){
     for(i in 1:narea){ ## will overwrite second time
       # calc area-specific SPB/R and Yield/R, using area-specific R
       SB_Ri[v,i] <- prop$SB_i[i]/(rec_level*rUse[i])
-      Yield_Ri[v,i] <- prop$Yield_i[i]/(rec_level*rUse[i])
+      Yield_Ri[v,i] <- prop$Yield_i[i]/(rec_level*rdistUse[i])
       ## Calc recruits using area-specific SB etc
       propEq <- Equil_Spawn_Recr_Fxn(steepness = steep, SSB_virgin = SB0_i[i],
-                                     Recr_virgin = R0*rUse[i], SPR_temp = SB_Ri[v,i])
+                                     Recr_virgin = R0*rdistUse[i], SPR_temp = SB_Ri[v,i])
       B_eq_i[v,i] <- propEq$B_equil
       R_eq_i[v,i] <- propEq$R_equil
-      if(v ==75) cat(k," ",i," ",B_eq_i[v,i], "\n")
-      # if(k == 2){ ## store quantities 
-      #   proposed_i[v,'Fv',i] <- Ftest[v]
-      #   proposed_i[v,'Yield',i] <-  Yield_Ri[v,i]*R_eq_i[v,i]
-      #   proposed_i[v,'B',i] <-   SB_Ri[v,i]*B_eq_i[v,i]
-      # } ## end k == 2 
+      # if(v ==75 ) cat(k," ",i," ",B_eq_i[v,i], "\n")
+      if(k == 2){ ## store quantities
+        proposed_i[v,'Fv',i] <- Ftest[v]
+        proposed_i[v,'Yield',i] <-  Yield_Ri[v,i]*R_eq_i[v,i]
+        proposed_i[v,'B',i] <-    SB_Ri[v,i] *R_eq_i[v,i]
+      } ## end k == 2
     } ## end areas    
     # use ratio of B_equils among areas to calculate adjustment to recr_dist 
-    if(k == 1) recr_dist_adj <- B_eq_i[v,]/sum(B_eq_i[v,]); next()
+    if(k == 1) recr_dist_adj <- B_eq_i[v,]/sum(B_eq_i[v,]) ; next()
+    # if(v > 100 ) cat(Ftest[v]," ",recr_dist_adj, "\n"); next()
     # if(k == 1) recr_dist_adj <- B_eq_i[v,]/sum(B_eq_i[v,])*recr_dist; next()
     
   } ## end steps a/b loop
 
   proposed[v,'Fv'] <- Ftest[v]
   proposed[v,'Yield'] <- sum(Yield_Ri[v,] * R_eq_i[v,]) # sum(Yield_Ri[v,]) * sum(R_eq_i[v,])
-  proposed[v,'B'] <-  sum(SB_Ri[v,] * B_eq_i[v,])  #sum(SB_Ri[v,]) * sum(B_eq_i[v,]) 
+  proposed[v,'B'] <-  sum(SB_Ri[v,] *  R_eq_i[v,i])  #sum(SB_Ri[v,]) * sum(B_eq_i[v,]) 
 } ## end FV
 
 
@@ -114,9 +117,23 @@ ggplot(current, aes(x = Fv, y = Yield)) +
   geom_line(data = proposed, lwd = 1.1, aes(color = 'proposed')) + 
   theme_sleek()
 
-
+ggplot(current, aes(x = Fv, y = B)) + 
+  geom_line(lwd = 1.1, aes(color = 'current')) + 
+  geom_line(data = proposed, lwd = 1.1, aes(color = 'proposed')) + 
+  theme_sleek()
 
 ggplot(current, aes(x = B, y = Yield)) + 
   geom_line(lwd = 1.1, aes(color = 'current')) + 
   geom_line(data = proposed, lwd = 1.1, aes(color = 'proposed')) + 
   theme_sleek()
+
+
+## AREA SPECIFIC YIELD CURVES
+plot(proposed_i[,,3], ylim = c(0,100))
+points(proposed_i[,,2])
+points(proposed_i[,,1])
+  
+plot(proposed_i[,c(1,3),3], ylim = c(0,500), col = 'red')
+points(proposed_i[,c(1,3),2])
+points(proposed_i[,c(1,3),1])
+
