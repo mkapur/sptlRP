f039cf2d814a126d9c90c9106bb8eb52289c0fea
Maia Sosa Kapur <kapurm@uw.edu>
2020-07-08 09:37:53 -0700

    store SRR from both approaches

--- rickMasterv2.R
+++ rickMasterv2.R
@@ -33,32 +33,28 @@ SB0 <- doNage( Fv = rep(0,narea),
                rdist = recr_dist,
                refR = rec_level)$SB_total
 
-## Get NAA using movement. Input X_ija to turn movement OFF (smooth curve)
+## Get NAA using movement. Input X_ija_NULL to turn movement OFF (smooth curve)
 ## applying system-wide F
 Ftest <- seq(0,1,0.005)
 current <- data.frame(Fv = NA, Yield = NA, B = NA)
+rick <- data.frame() ## storage for SRR question
 for(v in 1:length(Ftest)){
-  
   ## get values at present Fv
   curr <- doNage( Fv = rep(Ftest[v],narea), 
                   X = X_ija,
                   rdist = recr_dist, ## these are set to 1
                   refR = rec_level)
-  
   # calc SPB/R and Yield/R
   SB_R <- curr$SB_total/sum(rec_level)
   Yield_R <- curr$Yield_total/sum(rec_level)
-  
   #call Equil_Spawn_Recr_Fxn to get B_equil and R_equil from SPB/R and SR parms
   currEq <- Equil_Spawn_Recr_Fxn(steepness = steep[1], SSB_virgin = SB0, 
                                  Recr_virgin = sum(R0), SPR_temp = SB_R)## L17247 ON TPL
   current[v,'Fv'] <- Ftest[v]
   current[v,'Yield'] <- Yield_R * currEq$R_equil
   current[v,'B'] <- SB_R* currEq$R_equil#*currEq$B_equil
+  rick[v,'R_ESUMB'] <- currEq$R_equil ## expected recruits given total
 }
-
-
-
 
 ## proposed approach ----
 
@@ -66,7 +62,7 @@ for(v in 1:length(Ftest)){
 proposed <- data.frame(Fv = NA, Yield = NA, B = NA)
 proposed_i <- array(NA, dim = c(length(Ftest),3,narea), dimnames = list(NULL,c('Fv','Yield',"B"))) ## now for each area
 B_eq_i <- R_eq_i <- B_eq_i_INIT <- R_eq_i_INIT <- SB_Ri <- Yield_Ri<- matrix(NA, nrow =length(Ftest), ncol = narea)
-rick <- data.frame()
+
 maxiter = 100
 radj <- array(NA, dim = c(maxiter,length(Ftest),3)) ## keeping track of convergence
 
@@ -102,7 +98,6 @@ for(v in 1:length(Ftest)){
                     refR = rlevelUse) 
     
     # call Equ_Spawn_Recr_Fxn for each area to get B_equil and R_equil from SPB/R and SR parms
-    
     for(i in 1:narea){ ## will overwrite second time
       # calc area-specific SPB/R and Yield/R, using area-specific R
       if(length(rlevelUse) > 1){
@@ -118,37 +113,27 @@ for(v in 1:length(Ftest)){
                                      Recr_virgin = R0[i], SPR_temp = SB_Ri[v,i])
       B_eq_i[v,i] <- propEq$B_equil
       R_eq_i[v,i] <- propEq$R_equil ## gets overwritten each iteration
- 
-      # if(v == 44) cat(paste(k,SB_Ri[v,i] *R_eq_i[v,i]),"\n")
+
       if(k == maxiter){ ## store quantities
         proposed_i[v,'Fv',i] <- Ftest[v]
         proposed_i[v,'Yield',i] <-  Yield_Ri[v,i]*R_eq_i[v,i]
         proposed_i[v,'B',i] <-    SB_Ri[v,i] *R_eq_i[v,i]
-      } ## end k >1
+      } ## end k max
     } ## end areas    
     
-    # if(k >= 1){
-      
-      # use ratio of B_equils among areas to calculate adjustment to recr_dist 
-      # recr_dist_adj <- B_eq_i[v,]/sum(B_eq_i[v,]);
-      # cat(k,recr_dist_adj,"\n")
-  
+    if(k == maxiter){ ## store quantities
       ## storing info, not currently used
-      # rick[v,"Fv"] <- Ftest[v]
-      # rick[v,"SBeqtotal"] <-   sum(B_eq_i[v,] )
+      rick[v,"Fv"] <- Ftest[v]
+      rick[v,"SBeqtotal"] <-   sum(B_eq_i[v,] )
       # ## two options for calculating new R ref
       # ## sum of expected recruits in areas
-      # rick[v,"R_SUMEBA"]  <- sum( R_eq_i[v,]) 
+      rick[v,"R_SUMEBA"]  <- sum( R_eq_i[v,])
       # ## expected recruits given summed biomass in area
-      # rick[v,"R_ESUMB"] <- Equil_Spawn_Recr_Fxn(steepness = steep, SSB_virgin = sum(SB0_i),
-      #                                           Recr_virgin = R0, SPR_temp = sum(SB_Ri[v,]))$R_equil 
-    # }
-    # next()
-
+      # rick[v,"R_ESUMB"] <- Equil_Spawn_Recr_Fxn(steepness = steep[1], SSB_virgin = sum(SB0_i),
+      #                                           Recr_virgin = sum(R0), SPR_temp = sum(SB_Ri[v,]))$R_equil
+    }
   } ## end k:maxiter
-  
-  
   proposed[v,'Fv'] <- Ftest[v]
-  proposed[v,'Yield'] <-  sum(proposed_i[v,'Yield',]) #sum(Yield_Ri[v,] * R_eq_i[v,]) # sum(Yield_Ri[v,]) * sum(R_eq_i[v,])
+  proposed[v,'Yield'] <-   sum(proposed_i[v,'Yield',])# sum(Yield_Ri[v,] * R_eq_i[v,]) # sum(Yield_Ri[v,]) * sum(R_eq_i[v,])
   proposed[v,'B'] <-  sum(proposed_i[v,'B',])#sum(SB_Ri[v,] *  R_eq_i[v,i])  #sum(SB_Ri[v,]) * sum(B_eq_i[v,]) 
 } ## end FV
