3808a9115675d81c73b5ddb48b90978669115832
Maia Sosa Kapur <kapurm@uw.edu>
2020-07-27 09:05:52 -0700

    test alpha-beta setup
    this does step thru h, R0 to get a/b which match the other proposed method; in practice users would just hand it a,b (fixed throughout sim)

---
+++ R/abloop.R
@@ -0,0 +1,107 @@
+
+
+abloop <- function(Fv_i,
+                        rec_level_idx = 1,
+                        movemat = X_ija){
+
+  radj <- matrix(NA, nrow = maxiter, ncol = narea)
+  
+  
+  ## ORIGINAL APPROACH - USER PROVIDED R0_I
+  R0 <- R0_list[[rec_level_idx]]
+  rec_level <- R0
+
+  SB0_i <- doNage(Fv = rep(0,narea), 
+                  X = movemat,
+                  rdist = recr_dist,
+                  refR = rec_level)$SB_i
+  
+  ## get alpha, beta to match inputs
+  ## alpha = max recruits asymptoptes 1/b, b is ssb needed to produce half
+
+  ## fixed for each sim -- did this to match previous sims
+  alph = (SB0_i/R0)*(1-steep)/(4*steep)
+  bet = (5*steep-1)/(4*steep*R0)
+  
+  
+  last_req <- first_req <- yield_FI <- B_FI <- SBPR_i <- YPR_i <- NULL
+  for(k in 1:maxiter){ ## Loop over steps A & B
+    
+    if(k == 1){
+      rdistUse <- recr_dist ## no distribution now; full rec-level in each area
+      rlevelUse = rec_level ## pre-specified No recruits in area, currently R0
+    } else{
+      rdistUse <- recr_dist ## only after computing R_i
+      # rlevelUse = last_req + (1*rec_level) #last_req# round(last_req,2)# last_req  # c(R_eq_i[v,1:2], max(1,round(R_eq_i[v,3],0)))
+      rlevelUse = last_req #+ rec_level
+    }
+    
+    prop <- doNage( Fv = Fv_i, 
+                    X = movemat,
+                    rdist = rdistUse,
+                    refR = rlevelUse) 
+    
+    
+    # call Equ_Spawn_Recr_Fxn for each area to get B_equil and R_equil from SPB/R and SR parms
+    for(i in 1:narea){ ## will overwrite second time
+      # calc area-specific SPB/R and Yield/R, using area-specific R
+      
+      if( k > 1){
+        rleveltmp = rlevelUse[i] #min(, R0[i]) if unconstrained will be ~10 over R0
+      } else{
+        rleveltmp = rlevelUse[i]
+      }
+      # if(round(rleveltmp) == 0) next() ## end iteration if rec is now zero
+      # cat(v, k,i,rleveltmp,"\n")
+      # radj[k,v,i] <- rleveltmp
+      radj[k,i] <- rleveltmp ## store this
+      
+      
+      # rRef_proposed_radj[k,v,i,RR] <- rleveltmp
+      # SBPR_i[i] <-  prop$SB_i[i]/(rleveltmp*rdistUse[i]) ## Rick's idea
+      SBPR_i[i] <-  prop$SB_i[i]/rleveltmp#+0.005*R0[i]) ## Rick's idea
+
+      YPR_i[i] <- prop$Yield_i[i]/(rleveltmp)
+      # YPR_i[i] <- prop$Yield_i[i]/((rleveltmp+0.005*R0[i]))
+      # cat(k,i, YPR_i[i],"\n")
+      
+      ## Calc area-specific recruits using area-specific SB etc
+      # propEq <- abSRR(alpha = a[i], beta = b[i], biomass = prop$SB_i[i])
+      propEq <- abSRR(alpha = alph[i], 
+                      beta = bet[i], 
+                      biomass = prop$SB_i[i])
+      
+      
+      # if(k == 1) first_req[i] <- propEq$R_equil
+      # B_eq_i[v,i] <- propEq$B_equil
+      last_req[i] <- propEq$R_equil ## gets overwritten each iteration
+    } ## end areas
+    # cat(Ftest[Fv],k, YPR_i, SBPR_i,last_req,"\n")
+    
+    # cat(k,radj[k,],"\n")
+    if(k > 20){
+      if(all(round(abs(radj[k,]/radj[k-1,] - 1),2)  <=  0.05) |  k == maxiter){
+        for (i in 1:narea) {
+          yield_FI[i] <-  YPR_i[i] *  last_req[i]
+          B_FI[i] <-    SBPR_i[i] *  last_req[i]
+          cat(Ftest[Fv],k, i,  last_req[i], B_FI[i], yield_FI[i], "\n")
+          # cat(Ftest[Fv],k, i,  last_req[i], B_FI[i], yield_FI[i], "\n")
+        } ## end areas
+
+        break("maxiter reached ",i,k)
+      } ## end if neither 1%
+    } ## end K check
+
+  } ## end k:maxiter
+
+  ## save totals from final iteration
+  return(list(
+    Yield = sum(yield_FI),
+    Biomass = sum(B_FI),
+    Yield_i = yield_FI,
+    Biomass_i = B_FI,
+    radj = radj,
+    last_req = last_req, ## what was used for Yield Calc
+    last_YPR = YPR_i ## ratio mult for Yield
+  ))
+}
