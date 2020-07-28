35b15bb6f992c2d332950f54f50627073507dd43
Maia Sosa Kapur <kapurm@uw.edu>
2020-07-28 11:06:35 -0700

    correct this to match what was done in hw3, and also max 0 so no negs

--- R/abSRR.R
+++ R/abSRR.R
@@ -1,6 +1,7 @@
-abSRR <- function(alpha, beta, biomass){
+abSRR <- function(alpha, beta, SPR_temp){
   ## should give you R0 when input B0
   # return(list("R_equil" = alpha*biomass/(beta+biomass))) 
-  return(list("R_equil" = biomass/(alpha + beta*biomass))) 
+  # return(list("R_equil" = biomass/(alpha + beta*biomass))) ## textbook version
+  return(list("R_equil" = max(0,(SPR_temp - alpha)/(beta*SPR_temp))))
   
 }
