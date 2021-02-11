8a420ee3e289dbb72bfb028bf61256750802c06b
Maia Sosa Kapur <kapurm@uw.edu>
2021-02-11 11:04:11 -0800

    Update figs.R

--- R/figs.R
+++ R/figs.R
@@ -107,3 +107,36 @@ ggplot(iso1, aes(x = FF_Area1, y = yield, color = Area)) +
 ggsave(last_plot(),
        height = 10, width = 8, dpi = 520,
        file = here('figs',paste0(Sys.Date(),"-Isocline_Total.png")))
+
+
+  
+ggplot(propmsy, aes(y = FMSY,x = Fprop)) +
+  geom_line(lwd = 1.1) +
+  scale_y_continuous(limits = c(0.5,1)) +
+  theme_sleek() +
+  labs(x = 'Proportion F applied to Area 1', y = 'FMSY')
+ggsave(last_plot(), file = here('figs',paste0(Sys.Date(),'propFvsMSY.png')))
+
+
+out2 %>%
+  ggplot(., aes(y = FMSY,x = Fprop, color =tyield )) +
+  geom_point() +
+  scale_y_continuous(limits = c(0.5,1)) +
+  scale_color_viridis_c(na.value = 'white') +
+  theme_sleek() +
+  labs(x = 'Proportion F applied to Area 1', y = 'FMSY', color = 'Total Yield') +
+  geom_vline(xintercept = out2[which.max(out2$tyield),'Fprop'], linetype = 'dashed' ) +
+  annotate('text', 
+           x = out2[which.max(out2$tyield),'Fprop']*1.1,
+           y = out2[which.max(out2$tyield),'FMSY']*1.1, 
+           size = 3,
+           color ='seagreen',
+           label = as.expression(bquote(MSY~"="~.(round(out2[which.max(out2$tyield),'tyield']))))) +
+  annotate('text', 
+           x = out2[which.max(out2$tyield),'Fprop']*1.1,
+           y = out2[which.max(out2$tyield),'FMSY']*1.09, 
+           size = 3,
+           color ='seagreen',
+           label = as.expression(bquote(F[MSY]~"="~.(round(out2[which.max(out2$tyield),'FMSY'],2))))) +
+  
+  ggsave(last_plot(), file = here('figs',paste0(Sys.Date(),'propFvsMSY_tyield.png')))
