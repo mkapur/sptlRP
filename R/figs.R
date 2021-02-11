790ad9208ad6ac31fa8062d4ee1ecd91edc226e9
Maia Sosa Kapur <kapurm@uw.edu>
2021-02-11 10:38:45 -0800

    na value white

--- R/figs.R
+++ R/figs.R
@@ -1,3 +1,5 @@
+out[out < 0] <- NA
+
 FSB <- out %>%
   select(FF_Area1,FF_Area2,SB_A1,SB_A2) %>%
   reshape2::melt(id = c("FF_Area1","FF_Area2")) %>%
@@ -8,7 +10,7 @@ FSB <- out %>%
   coord_equal() +
   ggsidekick::theme_sleek() + 
   scale_x_continuous(expand = c(0,0)) + scale_y_continuous(expand = c(0,0)) +
-  scale_fill_viridis_c() +
+  scale_fill_viridis_c(na.value = 'white' ) +
   facet_wrap(~Area) +
   labs(x = 'F in Area 1', y = 'F in Area 2', fill = 'SB in area') 
 
@@ -22,7 +24,7 @@ FYIELD <- out %>%
   coord_equal() +
   ggsidekick::theme_sleek() + 
   scale_x_continuous(expand = c(0,0)) + scale_y_continuous(expand = c(0,0)) +
-  scale_fill_viridis_c(option = 'magma') +
+  scale_fill_viridis_c(option = 'magma',na.value = 'white') +
   facet_wrap(~Area) +
   labs(x = 'F in Area 1', y = 'F in Area 2', fill = 'Yield in area') 
 
@@ -45,7 +47,7 @@ FSB <- out %>%
   ggsidekick::theme_sleek() + 
   scale_x_continuous(expand = c(0,0)) + 
   scale_y_continuous(expand = c(0,0)) +
-  scale_fill_viridis_c() +
+  scale_fill_viridis_c(na.value = 'white') +
   labs(x = 'F in Area 1', 
        y = 'F in Area 2', 
        fill = 'Total SB') 
@@ -61,14 +63,14 @@ FYIELD <- out %>%
   coord_equal() +
   ggsidekick::theme_sleek() + 
   scale_x_continuous(expand = c(0,0)) + scale_y_continuous(expand = c(0,0)) +
-  scale_fill_viridis_c(option = 'magma') +
+  scale_fill_viridis_c(option = 'magma',na.value = 'white') +
   labs(x = 'F in Area 1', 
        y = 'F in Area 2',
        fill = 'Total Yield') 
 
 
 
-FSB|FYIELD
+FSB/FYIELD
 ggsave(last_plot(),
        height = 10, width = 8, dpi = 520,
        file = here('figs',paste0(Sys.Date(),"-FvsSBandYield_Total.png")))
@@ -95,9 +97,13 @@ ggplot(iso1, aes(x = FF_Area1, y = yield, color = Area)) +
   geom_line(lwd = 1.1) +
   geom_line(data = iso2, aes(x = FF_Area2), lwd = 1.1) +
   ggsidekick::theme_sleek() + 
-  scale_x_continuous(expand = c(0,0)) + scale_y_continuous(expand = c(0,0)) +
+  scale_x_continuous(expand = c(0,0)) + 
+  scale_y_continuous(expand = c(0,0)) +
   scale_color_manual(values = c('grey44','blue'), labels = c('Area 1','Area 2')) +
   labs(x = 'F', title = 'Yield Isoclines by Area',
-       subtitle = 'Alternative Area F = 0.05',
+       subtitle = 'Alternative Area F = 0.05; removed runs with any negative yield',
        y = 'Yield', color = '') 
 
+ggsave(last_plot(),
+       height = 10, width = 8, dpi = 520,
+       file = here('figs',paste0(Sys.Date(),"-Isocline_Total.png")))
