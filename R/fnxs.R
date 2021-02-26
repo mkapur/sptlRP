7232fecf7c32fa5959a9a9cc520b12e557ff886f
Maia Sosa Kapur <kapurm@uw.edu>
2021-02-26 10:06:50 -0800

    save datplot with scenario name

--- R/fnxs.R
+++ R/fnxs.R
@@ -1,5 +1,172 @@
 ## functions used
 
+makeOut2 <- function(propmsy){
+  
+  ## now take what's determined to be FMSY  and return ssb, yield etc
+  ## this is telling us where the best yield actually occurs, as a function of both
+  out2 <- array(NA, dim = c(nrow(propmsy),17,2), 
+                dimnames = list(c(rep(NA, nrow(propmsy))),c("Fprop", "FMSY",
+                                                            "FF_Area1","FF_Area2",
+                                                            "estRbar","estRprop",
+                                                            "Yield_A1","Yield_A2",
+                                                            "SB_A1","SB_A2",
+                                                            "SB0_A1","SB0_A2",
+                                                            "expR_A1","expR_A2",
+                                                            "obsR_A1","obsR_A2", 'tyield'),
+                                c('new','old'))) ## each slice is old or new
+  for(i in 1:nrow(propmsy)){
+    out2[i,'Fprop',1:2] <- propmsy[i,'Fprop']
+    out2[i,'FMSY',1] <- propmsy[i,'FMSY_new']
+    out2[i,'FMSY',2] <- propmsy[i,'FMSY_global']
+    FFs_new <- c(propmsy[i,'Fprop']*propmsy[i,'FMSY_new'],(1-propmsy[i,'Fprop'])*propmsy[i,'FMSY_new'] )
+    FFs_global <- c(propmsy[i,'Fprop']*propmsy[i,'FMSY_global'],(1-propmsy[i,'Fprop'])*propmsy[i,'FMSY_global'] )
+    
+    out2[i,'FF_Area1',1] <- FFs_new[1]; out2[i,'FF_Area2',1] <- FFs_new[2]
+    out2[i,'FF_Area1',2] <- FFs_global[1]; out2[i,'FF_Area2',2] <- FFs_global[2]
+    
+    opt0 <- optim_loop(FFs_new,i = NA) ## already specified
+    opt_temp <- opt0$opt_temp; tmp0 <- opt0$tmp0; tmp <- opt0$tmp
+    
+    out2[i,'estRbar',1] <- opt_temp$par[1];  out2[i,'estRprop',1] <- opt_temp$par[2];
+    out2[i,'estRbar',2] <- R0_global;  out2[i,'estRprop',2] <- Rprop_input
+    ## derived quants at optimized value
+    yields <- as.numeric(getYield(passR = out2[i,'estRbar',1], passRprop =   out2[i,'estRprop',1], YPR_F = tmp$YPR))
+    out2[i,'Yield_A1',1] <- yields[1];  out2[i,'Yield_A2',1] <- yields[2];
+    
+    sbs <-getSB(passR = out2[i,'estRbar',1], passRprop = out2[i,'estRprop',1], SBPR_F = tmp$SBPR)
+    out2[i,'SB_A1',1] <-  as.numeric(sbs[1]);  out2[i,'SB_A2',1] <-  as.numeric(sbs[2]);
+    
+    sb0 <- getSB(passR = out2[i,'estRbar',1], passRprop = out2[i,'estRprop',1], SBPR_F = tmp0$SBPR)
+    out2[i,'SB0_A1',1] <-  as.numeric(sb0[1]);  out2[i,'SB0_A2',1] <-  as.numeric(sb0[2]);
+    
+    rexp <- as.numeric(getExpR(passR = out2[i,'estRbar',1], 
+                               passRprop = out2[i,'estRprop',1],
+                               SB_F = sbs, SB_0 =sb0))
+    out2[i,'expR_A1',1] <- rexp[1];  out2[i,'expR_A2',1] <- rexp[2];
+    
+    obsr <- as.numeric(out2[i,'estRbar',1]*c(out2[i,'estRprop',1],1-out2[i,'estRprop',1]))
+    out2[i,'obsR_A1',1] <- obsr[1];  out2[i,'obsR_A2',1] <- obsr[2];
+    rm(opt0)
+    ## derived quants at global value ("current method")
+    yields <- as.numeric(getYield(passR = out2[i,'estRbar',2], passRprop =   out2[i,'estRprop',2], YPR_F = tmp$YPR))
+    out2[i,'Yield_A1',2] <- yields[1];  out2[i,'Yield_A2',2] <- yields[2];
+    
+    sbs <-getSB(passR = out2[i,'estRbar',2], passRprop = out2[i,'estRprop',2], SBPR_F = tmp$SBPR)
+    out2[i,'SB_A1',2] <-  as.numeric(sbs[1]);  out2[i,'SB_A2',2] <-  as.numeric(sbs[2]);
+    
+    sb0 <- getSB(passR = out2[i,'estRbar',2], passRprop = out2[i,'estRprop',2], SBPR_F = tmp0$SBPR)
+    out2[i,'SB0_A1',2] <-  as.numeric(sb0[1]);  out2[i,'SB0_A2',2] <-  as.numeric(sb0[2]);
+    
+    rexp <- as.numeric(getExpR(passR = out2[i,'estRbar',2], passRprop =out2[i,'estRprop',2],
+                               SB_F = data.frame(sbs), SB_0 =data.frame(sb0)))
+    out2[i,'expR_A1',2] <- rexp[1];  out2[i,'expR_A2',2] <- rexp[2];
+    
+    obsr <- as.numeric(out2[i,'estRbar',2]*c( out2[i,'estRbar',2],1-out2[i,'estRprop',2]))
+    out2[i,'obsR_A1',2] <- obsr[1];  out2[i,'obsR_A2',2] <- obsr[2];
+    
+    out2[i,'tyield',1] <- out2[i,'Yield_A1',1]+ out2[i,'Yield_A2',1]
+    out2[i,'tyield',2] <- out2[i,'Yield_A1',2]+ out2[i,'Yield_A2',2]
+    rm(tmp)
+  }
+  return(out2)
+}
+
+## calls from global environment to optimize
+getMSY <- function(){
+  
+  # https://stackoverflow.com/questions/57173162/function-for-uniroot-that-has-two-parameters-that-need-to-be-run-across-a-vector
+  ## the example above actually has 3 pars and he optimizes over 2 known vectors
+  ## the mapply will return the best F value given proportion
+  ## inside dfx.dxSYS_new we run optim and use passed R, Rprop
+  ## need a second version of this which uses global R, rprop
+  FpropVec <- seq(0.01,1,0.01) ## all possible proportions of F in Area 1
+  fbest_new <-
+    mapply(
+      function(Fv_prop)
+        uniroot(f = dfx.dxSYS_new, 
+                interval = c(0.02,5),
+                Fv_prop = Fv_prop)[1],
+      FpropVec)
+  
+  fbest_global  <-
+    mapply(
+      function(Fv_prop)
+        uniroot(f = dfx.dxSYS_global, 
+                interval = c(0.02,5),
+                Fv_prop = Fv_prop)[1],
+      FpropVec)
+  
+  propmsy <- data.frame('Fprop' = FpropVec,
+                        'FMSY_new' = matrix(unlist(fbest_new)),
+                        'FMSY_global' = matrix(unlist(fbest_global)))  
+  return(propmsy)
+}
+
+## run analysis across discrete surface given input LH and FF vetors
+makeOut <- function(dat,FFs){
+  
+  ## Generate a surface of expected yields, given new method
+  out <- array(NA, dim = c(nrow(FFs),15,2), 
+               dimnames = list(c(1:nrow(FFs)),c("FF_Area1","FF_Area2",
+                                                "estRbar","estRprop",
+                                                "Yield_A1","Yield_A2",
+                                                "SB_A1","SB_A2",
+                                                "SB0_A1","SB0_A2",
+                                                "expR_A1","expR_A2",
+                                                "obsR_A1","obsR_A2","tyield"),
+                               c('new','old'))) ## each slice is old or new
+  
+  for(i in 1:nrow(FFs)){
+    out[i,'FF_Area1',] <- FFs[i,1];   out[i,'FF_Area2',] <- FFs[i,2]
+    
+    ## this is the new method; old method uses global inputs
+    opt0 <- optim_loop(FFs,i) 
+    opt_temp <- opt0$opt_temp; tmp0 <- opt0$tmp0; tmp <- opt0$tmp
+    out[i,'estRbar',1] <- opt_temp$par[1];  out[i,'estRprop',1] <- opt_temp$par[2];
+    out[i,'estRbar',2] <- R0_global;  out[i,'estRprop',2] <- Rprop_input
+    
+    ## derived quants at optimized value
+    yields <- as.numeric(getYield(passR = out[i,'estRbar',1], passRprop =   out[i,'estRprop',1], YPR_F = tmp$YPR))
+    out[i,'Yield_A1',1] <- yields[1];  out[i,'Yield_A2',1] <- yields[2];
+    
+    sbs <-getSB(passR = out[i,'estRbar',1], passRprop = out[i,'estRprop',1], SBPR_F = tmp$SBPR)
+    out[i,'SB_A1',1] <- as.numeric(sbs[1]);  out[i,'SB_A2',1] <- as.numeric(sbs[2]);
+    
+    sb0 <- getSB(passR = out[i,'estRbar',1], passRprop = out[i,'estRprop',1], SBPR_F = tmp0$SBPR)
+    out[i,'SB0_A1',1] <- as.numeric(sb0[1]);  out[i,'SB0_A2',1] <-as.numeric(sb0[2]);
+    
+    rexp <- as.numeric(getExpR(passR = out[i,'estRbar',1], 
+                               passRprop = out[i,'estRprop',1],
+                               SB_F =sbs, SB_0 =sb0))
+    out[i,'expR_A1',1] <- rexp[1];  out[i,'expR_A2',1] <- rexp[2];
+    
+    obsr <- as.numeric(out[i,'estRbar',1]*c(out[i,'estRprop',1],1-out[i,'estRprop',1]))
+    out[i,'obsR_A1',1] <- obsr[1];  out[i,'obsR_A2',1] <- obsr[2];
+    rm(opt0)
+    
+    ## derived quants at global value ("current method")
+    yields <- as.numeric(getYield(passR = out[i,'estRbar',2], passRprop =   out[i,'estRprop',2], YPR_F = tmp$YPR))
+    out[i,'Yield_A1',2] <- yields[1];  out[i,'Yield_A2',2] <- yields[2];
+    
+    
+    sbs <-getSB(passR = out[i,'estRbar',2], passRprop = out[i,'estRprop',2], SBPR_F = tmp$SBPR)
+    out[i,'SB_A1',2] <- as.numeric(sbs[1]);  out[i,'SB_A2',2] <- as.numeric(sbs[2]);
+    
+    sb0 <- getSB(passR = out[i,'estRbar',2], passRprop = out[i,'estRprop',2], SBPR_F = tmp0$SBPR)
+    out[i,'SB0_A1',2] <- as.numeric(sb0[1]);  out[i,'SB0_A2',2] <-as.numeric(sb0[2]);
+    
+    rexp <- as.numeric(getExpR(passR = out[i,'estRbar',2], passRprop =   out[i,'estRprop',2],
+                               SB_F = data.frame(sbs), SB_0 =data.frame(sb0)))
+    out[i,'expR_A1',2] <- rexp[1];  out[i,'expR_A2',2] <- rexp[2];
+    
+    obsr <- as.numeric(out[i,'estRbar',2]*c( out[i,'estRbar',2],1-out[i,'estRprop',2]))
+    out[i,'obsR_A1',2] <- obsr[1];  out[i,'obsR_A2',2] <- obsr[2];
+    out[i,'tyield',1] <- out[i,'Yield_A1',1]+ out[i,'Yield_A2',1]
+    out[i,'tyield',2] <- out[i,'Yield_A1',2]+ out[i,'Yield_A2',2]
+    return(out)
+  }
+}
+
 ## takes F, performs optimization and returns derived quants at Rbar hat rprop hat
 optim_loop <- function(FFs,i){
   if(is.na(i)){ ## for uniroot
@@ -136,7 +303,7 @@ makeDat <- function(nage = 20, narea =2,
       
       dat[age,"proportion_stay",area] <- 1 ## recruits stay put
       ## do descending pstay from 1 (full sedentary) to Xija (whatever the terminal sedentary prop is)
-      if(age <10 & age > 1) dat[age,"proportion_stay",area] <- 1-age*(pStay[area])/length(1:12)+0.25
+      if(age <10 & age > 1) dat[age,"proportion_stay",area] <- 1+age*(1-pStay[area])/-9
         # min(c(pStay[area], age*(pStay[area])/length(1:12)+0.25))
       if(age >= 10) dat[age,"proportion_stay",area] <- pStay[area]
       if(all(pStay == 1)) dat[age,"proportion_stay",area] <- 1 ## no movement exception
@@ -150,7 +317,7 @@ makeDat <- function(nage = 20, narea =2,
     } ## end age
   } ## end area
   
-  png(here('figs',paste0(Sys.Date(),'-inputDat.png')),
+  png(here('figs',paste0(Sys.Date(),"-",SCENARIO,'-inputDat.png')),
       width = 8, height = 8, units = 'in', res = 400)
   par(mfrow = c(2,2))
   for(v in 2:5){
