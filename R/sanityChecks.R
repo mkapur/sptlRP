8b0c8da9815b55d54a346a0192c1d66060a484e4
Maia Sosa Kapur <kapurm@uw.edu>
2021-03-08 14:23:20 -0800

    Update sanityChecks.R

--- R/sanityChecks.R
+++ R/sanityChecks.R
@@ -7,27 +7,28 @@ plot(cbind(1,bh(h=steep, prop = Rprop_input, r0 = R0_global, b0 = 15, bcurr = 1)
 for(i in 1:100)points(cbind(i,bh(h=steep, prop = Rprop_input, r0 = R0_global, b0 = 15, bcurr = i)))
 dev.off()
 
-png(here('figs','testdoPR-movement.png'))
-par(mfrow = c(2,2))
-dat <- makeDat(wa = c(5,5),
-               fec_a50 = c(6,6),
-               fec_a95 = c(12,12),
-               slx_a50 = c(9,9),
-               slx_a95 = c(13,13),
-               pStay = c(0.9,0.6))
+png(here('figs','testdoPR-movement.png'), width = 6, height = 4, units = 'in', res = 520)
+par(mfrow = c(1,2), mar = c(4,4,1,1))
+# dat <- makeDat(wa = c(5,5),
+#                fec_a50 = c(6,6),
+#                fec_a95 = c(12,12),
+#                slx_a50 = c(9,9),
+#                slx_a95 = c(13,13),
+#                pStay = c(0.9,0.6))
 tmp <- doPR(dat, FF = c(0,0)) ## defaults, no fishing
 all(round(colSums(tmp$NPR[,,1])) == round(colSums(tmp$NPR[,,2]))) ## net exchange should be conserved
-plot(tmp$NPR[1,,1], main = 'A1 SRC - defaults');points(tmp$NPR[2,,1],col = 'blue') ## should look reasonable
+plot(tmp$NPR[1,,1], main = 'Spawned in A1', xlab = 'Age', ylab = 'Numbers per 1 Recruit' );points(tmp$NPR[2,,1],col = 'blue') ## should look reasonable
 legend('topright', pch =1, legend = c('stay in a1', 'end up in a2'), col = c('black','blue'))
-plot(tmp$NPR[1,,2], main = 'A2 SRC - defaults', ylim = c(0,1));points(tmp$NPR[2,,2],col = 'blue') ## should look reasonable
+plot(tmp$NPR[1,,2], main = 'Spawned in A2', ylim = c(0,1), xlab = 'Age', 
+     ylab = '' );points(tmp$NPR[2,,2],col = 'blue') ## should look reasonable
 legend('topright', pch =1, legend = c('stay in a2', 'end up in a1'), col = rev(c('black','blue')))
 ## test run with no movement
-dat <- makeDat(wa = c(5,5),
-               fec_a50 = c(6,6),
-               fec_a95 = c(12,12),
-               slx_a50 = c(9,9),
-               slx_a95 = c(13,13),
-               pStay = c(1,1))
+# dat <- makeDat(wa = c(5,5),
+#                fec_a50 = c(6,6),
+#                fec_a95 = c(12,12),
+#                slx_a50 = c(9,9),
+#                slx_a95 = c(13,13),
+#                pStay = c(1,1))
 tmp <- doPR(dat, FF = c(0,0)) ## defaults, no fishing
 all(round(colSums(tmp$NPR[,,1])) == round(colSums(tmp$NPR[,,2]))) ## net exchange should be conserved
 plot(tmp$NPR[1,,1], main = 'A1 SRC - no movement', ylim = c(0,1));points(tmp$NPR[2,,1],col = 'blue') 
