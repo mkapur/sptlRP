352de52b0e21a4a5a05f3324806a9af88a70a0b8
Maia Sosa Kapur <kapurm@uw.edu>
2021-07-03 09:56:44 -0700

    Update figs.R

--- R/figs.R
+++ R/figs.R
@@ -7,43 +7,43 @@ dev.off()
 
 
 out_use %>% mutate(SB_A1+ SB_A2) %>% head()
-outold %>% mutate(SB_A1+ SB_A2) %>% head()
+outglobal %>% mutate(SB_A1+ SB_A2) %>% head()
 
-out2_new <- data.frame(out2[,,'new'])
-out2_global <- data.frame(out2[,,'old'])
+out2_local <- data.frame(surface[,,'local'])
+out2_global <- data.frame(surface[,,'global'])
 
-out_use <- data.frame(out[,,'new']) 
-outold <- data.frame(out[,,'old']) 
+out_use <- data.frame(surface[,,'local']) 
+outglobal <- data.frame(surface[,,'global']) 
 
 ## Isocline yield curve ----
 
-iso_new <- data.frame(out[,,'new']) %>%
+iso_local <- data.frame(surface[,,'local']) %>%
   filter( round(FF_Area2,1) == 
-            round(out2_new[which.max(out2_new[,'tyield']),c('FF_Area2')],1) ) %>%
+            round(out2_local[which.max(out2_local[,'tyield']),c('FF_Area2')],1) ) %>%
   select(FF_Area1,FF_Area2,tyield) %>%
   reshape2::melt(id = c("FF_Area1","FF_Area2")) %>%
   mutate(Area = substr(variable,7,8), yield = value) %>%
   select(-variable,-value)
 
-iso_old <- data.frame(out[,,'old']) %>%
+iso_global <- data.frame(surface[,,'global']) %>%
   filter( round(FF_Area2,1) == 
-            round(out2_new[which.max(out2_new[,'tyield']),c('FF_Area2')],1) ) %>%
+            round(out2_local[which.max(out2_local[,'tyield']),c('FF_Area2')],1) ) %>%
   select(FF_Area1,FF_Area2,tyield) %>%
   reshape2::melt(id = c("FF_Area1","FF_Area2")) %>%
   mutate(Area = substr(variable,7,8), yield = value) %>%
   select(-variable,-value) 
 
-ggplot(data = iso_new, aes(y = yield)) +
+ggplot(data = iso_local, aes(y = yield)) +
   geom_line(aes(x = FF_Area1, linetype = 'LOCAL', color = 'LOCAL'), lwd = 1.1) +
-  geom_line(data = iso_old, aes(x = FF_Area1, linetype = 'GLOBAL', color = 'GLOBAL'), lwd = 1.1) +
+  geom_line(data = iso_global, aes(x = FF_Area1, linetype = 'GLOBAL', color = 'GLOBAL'), lwd = 1.1) +
   ggsidekick::theme_sleek() +
-  geom_vline(aes(xintercept = out2_new[which.max(out2_new[,'tyield']),c('FF_Area1')],
+  geom_vline(aes(xintercept = out2_local[which.max(out2_local[,'tyield']),c('FF_Area1')],
                  lty = 'LOCAL', color = 'LOCAL')) +
   geom_vline(aes(xintercept = out2_global[which.max(out2_global[,'tyield']),c('FF_Area1')],
                  lty = 'GLOBAL', color = 'GLOBAL')) +
   scale_color_manual(values = c('black','blue'), labels = c('GLOBAL','LOCAL')) +
-  scale_x_continuous(limits = c(0,1.5)) +
-  scale_y_continuous(limits = c(0,1500)) +
+  # scale_x_continuous(limits = c(0,1.5)) +
+  # scale_y_continuous(limits = c(0,1500)) +
   labs(x = 'F in Area 1 (F Area 2 = FMSY Area 2)', 
        title = paste0(SCENARIO,' Yield Isocline Conditional on FA1'),
        subtitle = 'vertical line indicates FMSY in Area',
@@ -56,12 +56,12 @@ ggsave(last_plot(),
 ## Local assumption panel ----
 # https://stackoverflow.com/questions/24237399/how-to-select-the-rows-with-maximum-values-in-each-group-with-dplyr
 
-out_use <- data.frame(out[,,'new']) 
-# maxf1 <- out2_new[which.max(out2_new[,'tyield']),c('FF_Area1')]*1.5
+out_use <- data.frame(surface[,,'local']) 
+# maxf1 <- out2_local[which.max(out2_local[,'tyield']),c('FF_Area1')]*1.5
 # maxf1 = max(out_use$FF_Area1)
 maxf1 = 1.5
 
-new <- out_use %>%
+local <- out_use %>%
   select(FF_Area1,FF_Area2, Yield_Total =tyield) %>%  
   reshape2::melt(id = c("FF_Area1","FF_Area2")) %>%
   # mutate(Area = substr(variable,7,8), yield = value) %>%
@@ -78,33 +78,33 @@ new <- out_use %>%
   # scale_color_viridis_c(option = 'magma',na.value = 'white') +
   scale_fill_gradient2(low = "white", high = "grey11")+
   scale_color_gradient2()+
-  ## add the locations of FMSY from new method
-  # geom_point(data = out2_new, aes(x = FF_Area1, y = FF_Area2), 
+  ## add the locations of FMSY from local method
+  # geom_point(data = out2_local, aes(x = FF_Area1, y = FF_Area2), 
   #            fill = NA,
   #            size = 2, alpha = 0.3) +
-  geom_point(data = out2_new, aes(x = out2_new[which.max(out2_new[,'tyield']),'FF_Area1'],
-                                  y = out2_new[which.max(out2_new[,'tyield']),'FF_Area2']), 
-             fill = NA, color = 'gold', size = 2, pch =15)+
+  geom_point(data = out2_local, aes(x = out2_local[which.max(out2_local[,'tyield']),'FF_Area1'],
+                                  y = out2_local[which.max(out2_local[,'tyield']),'FF_Area2']), 
+             fill = NA, color = 'gglobal', size = 2, pch =15)+
   annotate('text',
            x =0.6*maxf1,
            y = 0.85*maxf1,
            size = 5,
-           color = 'gold',
+           color = 'gglobal',
            label = as.expression(bquote(MSY[Local]~
-                                          "="~.(round(out2_new[which.max(out2_new[,'tyield']),
+                                          "="~.(round(out2_local[which.max(out2_local[,'tyield']),
                                                                'tyield']))))) +
   annotate('text',
            x =0.6*maxf1,
            y = 0.8*maxf1,
            size = 5,
-           color ='gold',
-           label = as.expression(bquote(F[MSY_Local]~"="~.(round(out2_new[which.max(out2_new$tyield),'FF_Area1'],2))~"Area 1, "~.
-                                        (round(out2_new[which.max(out2_new$tyield),'FF_Area2'],2))~"Area 2"))) +
+           color ='gglobal',
+           label = as.expression(bquote(F[MSY_Local]~"="~.(round(out2_local[which.max(out2_local$tyield),'FF_Area1'],2))~"Area 1, "~.
+                                        (round(out2_local[which.max(out2_local$tyield),'FF_Area2'],2))~"Area 2"))) +
  
   labs(x = 'F in Area 1',   y = 'F in Area 2', fill = 'Total Yield',  title = SCENARIO) 
 
 ## global assumption panel ----
-out_use <- data.frame(out[,,'old']) 
+out_use <- data.frame(surface[,,'global']) 
 out_use[out_use < 0] <- 0
 # maxf1 <- out2_global[which.max(out2_global[,'tyield']),c('FF_Area1')]*1.5
 # maxf1 = max(out_use$FF_Area1)
@@ -129,12 +129,12 @@ global <- out_use %>%
   #            color = 'navy', size = 2, alpha = 0.3) +
   geom_point(data = out2_global, aes(x = out2_global[which.max(out2_global[,'tyield']),'FF_Area1'],
                                   y = out2_global[which.max(out2_global[,'tyield']),'FF_Area2']),
-             fill = NA, color = 'gold', size = 2, pch =15)+
+             fill = NA, color = 'gglobal', size = 2, pch =15)+
   annotate('text',
            x =0.6*maxf1,
            y = 0.85*maxf1,
            size = 5,
-           color ='gold',
+           color ='gglobal',
            label = as.expression(bquote(MSY[Global]~
                                           "="~.(round(out2_global[which.max(out2_global[,'tyield']),
                                                                'tyield']))))) +
@@ -142,12 +142,12 @@ global <- out_use %>%
            x =0.6*maxf1,
            y = 0.8*maxf1,
            size = 5,
-           color ='gold',
+           color ='gglobal',
            label = as.expression(bquote(F[MSY_Global]~"="~.(round(out2_global[which.max(out2_global$tyield),'FF_Area1'],2))~"Area 1, "~.
                                         (round(out2_global[which.max(out2_global$tyield),'FF_Area2'],2))~"Area 2"))) +
   labs(x = 'F in Area 1',   y = 'F in Area 2', fill = 'Total Yield',  title = SCENARIO) 
 
-ggsave(new    | global,
+ggsave(local    | global,
        height = 8, width = 10, dpi = 520,
        file =   paste0(filetemp,"/",SCENARIO,"-FvsYield_compare.png"))
 cat(paste0('saved ',Sys.Date(),"-",SCENARIO,"-FvsYield_compare.png", '\n'))
@@ -198,8 +198,8 @@ lines(colSums(tmp$NPR[,,2]),lwd = 2, col = 'blue', type = 'p') ## should look re
 legend('topright',lwd = 2, legend = c('Present in a2', 'Present in a1'), col = rev(c('black','blue')))
 text(x = 15, y = 0.7, label = SCENNAMES[s], cex = 1.5)
 
-FFvec <- c(out2_new[which.max(out2_new[,'tyield']),'FF_Area1'],
-           out2_new[which.max(out2_new[,'tyield']),'FF_Area2'])
+FFvec <- c(out2_local[which.max(out2_local[,'tyield']),'FF_Area1'],
+           out2_local[which.max(out2_local[,'tyield']),'FF_Area2'])
 
 tmp <- doPR(datlist[[s]], FF =FFvec)
 
